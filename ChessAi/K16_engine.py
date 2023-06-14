f0748debebf7b3196ee9ee367131eb9defa5dd84
radicore <86078764+radicore@users.noreply.github.com>
2023-06-14 21:18:03 +0100

    Update K16_engine.py

--- ChessAi/K16_engine.py
+++ ChessAi/K16_engine.py
@@ -4,8 +4,7 @@ import multiprocessing as mp
 
 from time import time
 from depth_handler import set_depth, optimal_move
-from evaluation import check_end_game
-
+from opening_handler import *
 
 ENGINE = "K16_2"  # Engine model - leave blank for default engine (2) or write "K16_2"
 
@@ -18,16 +17,16 @@ COMPUTER = chess.WHITE  # What the computer plays as
 PROCESSORS = mp.cpu_count()  # Using all CPU's for faster (multi) processing - manual setting may affect moves
 
 game = chess.pgn.Game()  # To save output as pgn
-
-board = chess.Board("7Q/7K/8/8/4k3/8/8/8 w - - 0 1")  # "3k4/7R/p2N1p2/8/8/8/PPP2PPP/2K4R b - - 0 27"
+book = book_to_array()
+board = chess.Board()
 
 
 def K16_move():
     TYPES = {"K16_1": 1, "K16_2": 2, "K16_BLEND": 3}; TYPE = 2  # default type 2 (faster version)
-    start = time()
     if ENGINE in TYPES: TYPE = TYPES[ENGINE]
     max_depth = set_depth(board, engineType=TYPE) if MAX_DEPTH is None else MAX_DEPTH
-    best_move, evaluation = optimal_move(max_depth, board, engineType=TYPE, end_game=check_end_game(board), processes=PROCESSORS, debug=True)
+    start = time()
+    best_move, evaluation = optimal_move(max_depth, board, engineType=TYPE, processes=PROCESSORS, debug=True)
     board.push(best_move)
     print(f"\nBot has played {best_move} with an evaluation of {evaluation} in {round(-(start - time()), 3)} seconds\n")
     print(board)
@@ -37,20 +36,36 @@ def K16_move():
 def player_move():
     while True:
         input_str = input("Your move: ")
-        try: move = board.parse_san(input_str); board.push(move); break
-        except ValueError: print("\nIllegal move. Try again.\n")
+        try:
+            move = board.parse_san(input_str); board.push(move); break
+        except ValueError:
+            print("\nIllegal move. Try again.\n")
     print(board); print(f"{ENGINE} Engine is thinking...")
 
 
 def play():
     print(board)
+    count = -2 if COMPUTER == chess.WHITE else 0
     while not (board.is_game_over() or board.is_stalemate() or board.is_repetition()):
         print("Board with fen: ", board.fen())
         if board.turn == COMPUTER:
-            K16_move()
+            count += 2
+
+            game_line = get_MM(chess.pgn.Game.from_board(board))
+            move = random_variation_move(book, game_line, count)
+
+            if move is not None:
+                board.push_san(move)
+                print("\n")
+                print(board)
+                print("\n")
+            else:
+                # print("====== END OF BOOK MOVES ======")
+                K16_move()
         else:
             player_move()
-            # K16_move()
+            # K16_move()  # for computer vs computer lol
+
 
 # cProfile.run("play()")  # debug purposes after run
 
