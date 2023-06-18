b3b00a67a280f82deee35d17197269e822e5f50c
radicore <86078764+radicore@users.noreply.github.com>
2023-06-18 15:29:40 +0100

    Update organize.py

--- ChessAi/organize.py
+++ ChessAi/organize.py
@@ -1,21 +1,24 @@
-import chess
+from chess import QUEEN, KNIGHT, ROOK, BISHOP
 
 
 def order_moves(BOARD):
     high_score_moves = []
     for move in BOARD.legal_moves:
-        if BOARD.is_capture(move) or BOARD.is_en_passant(move) or (move.promotion == chess.QUEEN or move.promotion == chess.KNIGHT) or BOARD.is_checkmate() or BOARD.is_check():
+        if BOARD.is_capture(move) or BOARD.is_en_passant(move) or (move.promotion in [QUEEN, KNIGHT, ROOK, BISHOP]) or BOARD.is_checkmate() or BOARD.is_check():
             if BOARD.is_checkmate():
                 return [move]
-            if BOARD.is_check():
+            elif BOARD.is_check():
                 high_score_moves.insert(0, move)
-            if move.promotion == chess.QUEEN:
+            elif BOARD.is_capture(move) or BOARD.is_en_passant(move):
                 high_score_moves.insert(1, move)
-            if BOARD.is_capture(move) or BOARD.is_en_passant(move):
+            elif move.promotion == QUEEN:
                 high_score_moves.insert(2, move)
-            if move.promotion == chess.KNIGHT:
+            elif move.promotion == KNIGHT:
                 high_score_moves.insert(3, move)
-            # ELSE other promotions are useless, so no need to add them (only need QUEEN or KNIGHT)
+            elif move.promotion == BISHOP:
+                high_score_moves.insert(4, move)
+            elif move.promotion == ROOK:
+                high_score_moves.insert(5, move)
         else:
             high_score_moves.append(move)  # scores that are not as high are added last
 
