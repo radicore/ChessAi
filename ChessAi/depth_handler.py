0254be2ada17a86d9e2817008aef909023b86aef
radicore <86078764+radicore@users.noreply.github.com>
2023-06-22 21:34:17 +0100

    Update depth_handler.py

--- ChessAi/depth_handler.py
+++ ChessAi/depth_handler.py
@@ -1,6 +1,8 @@
 import random
 import multiprocessing as mp
 
+import chess
+
 from minimax import minimax_AB
 from organize import order_moves
 from chess import SQUARES
@@ -14,13 +16,19 @@ def material_count(BOARD):  # adds up how many pieces there are all together (to
 def set_depth(BOARD, engineType=2):
     mc = material_count(BOARD)
     if engineType == 2:
-        if mc in [3, 4]: return 6
-        elif mc in [i for i in range(5, 16)]: return 5
-        else: return 4
+        if mc in [3, 4]:
+            return 6
+        elif mc in [i for i in range(5, 16)]:
+            return 5
+        else:
+            return 3
     else:
-        if mc in [3, 4]: return 6
-        elif mc in [i for i in range(5, 16)]: return 4
-        else: return 3
+        if mc in [3, 4]:
+            return 6
+        elif mc in [i for i in range(5, 16)]:
+            return 4
+        else:
+            return 3
 
 
 def minimax_worker(move, BOARD, depth, alpha, beta, turn, end_game, engineType, result_queue):
@@ -33,13 +41,20 @@ def optimal_move(max_depth, BOARD, end_game=False, engineType=2, debug=False, pr
 
     alpha = -INF
     beta = INF
-    
+
     best_eval = -INF if BOARD.turn else INF
+
+    step = 2
+
+    if BOARD.fen() in ["8/8/8/8/3K4/2R5/1k6/8 w - - 0 1", "8/1k6/2R5/3K4/8/8/8/8 w - - 0 1", "8/6k1/5R2/4K3/8/8/8/8 w - - 0 1", "8/8/8/8/4K3/5R2/6k1/8 w - - 0 1"]:
+        step = 10
+        print("DEPTH SET TO 10 (KNOWN CHECKMATE POSITION) Calculating moves...")
 
     with mp.Pool(processes=processes) as pool, mp.Manager() as manager:
         result_queue = manager.Queue()
         moves = order_moves(BOARD)
-        for depth in range(1, max_depth + 1):
+
+        for depth in range(0, max_depth + 1, step):
             for move in moves:
                 BOARD.push(move)
                 pool.apply_async(minimax_worker, args=(move, BOARD.copy(), depth, alpha, beta, BOARD.turn, end_game, engineType, result_queue))
@@ -50,8 +65,8 @@ def optimal_move(max_depth, BOARD, end_game=False, engineType=2, debug=False, pr
 
                 alpha = max(alpha, EVAL) if BOARD.turn else alpha
                 beta = min(beta, EVAL) if not BOARD.turn else beta
-
-                # I have a hint it causes a logic error as it only checks for maximizing the current side (finding max values only)
+                
+                true_depth = int(depth / 2)+1
 
                 if BOARD.turn:
                     if EVAL > best_eval:
@@ -60,7 +75,7 @@ def optimal_move(max_depth, BOARD, end_game=False, engineType=2, debug=False, pr
                         if EVAL == INF:
                             pool.terminate(), pool.join()
                             if debug:
-                                print(f"FORCED CHECKMATE FOUND IN {depth} MOVE(S): {move}")
+                                print(f"M{true_depth} MOVE(S) - {move}")
                             return move, EVAL
                 else:
                     if EVAL < best_eval:
@@ -69,7 +84,7 @@ def optimal_move(max_depth, BOARD, end_game=False, engineType=2, debug=False, pr
                         if EVAL == -INF:
                             pool.terminate(), pool.join()
                             if debug:
-                                print(f"FORCED CHECKMATE FOUND IN {depth} MOVE(S): {move}")
+                                print(f"M{true_depth} MOVE(S) - {move}")
                             return move, EVAL
 
             if best_move is None:
