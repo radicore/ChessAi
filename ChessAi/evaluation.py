b4d20c35835a0cefb300e9e0a206baf689c6af65
radicore <86078764+radicore@users.noreply.github.com>
2023-07-03 22:31:53 +0100

    Update evaluation.py

--- ChessAi/evaluation.py
+++ ChessAi/evaluation.py
@@ -25,26 +25,28 @@ PIECE_VALUES = {
 
 def square_mapping(piece, square, end_game):
     piece_type = piece.piece_type
-    mapping = []  # used to store square mapping (piece mapping tables)
+    _mapping = []  # used to store square mapping (piece mapping tables)
     if piece_type == chess.PAWN:
-        mapping = PAWNS_END_TABLE if end_game else PAWNS_TABLE
+        _mapping = PAWNS_END_TABLE if end_game else PAWNS_TABLE
     elif piece_type == chess.KNIGHT:
-        mapping = KNIGHTS_END_TABLE if end_game else KNIGHTS_TABLE
+        _mapping = KNIGHTS_END_TABLE if end_game else KNIGHTS_TABLE
     elif piece_type == chess.BISHOP:
-        mapping = BISHOPS_END_TABLE if end_game else BISHOPS_TABLE
+        _mapping = BISHOPS_END_TABLE if end_game else BISHOPS_TABLE
     elif piece_type == chess.ROOK:
-        mapping = ROOKS_END_TABLE if end_game else ROOKS_TABLE
+        _mapping = ROOKS_END_TABLE if end_game else ROOKS_TABLE
     elif piece_type == chess.QUEEN:
-        mapping = QUEENS_END_TABLE if end_game else QUEENS_TABLE
+        _mapping = QUEENS_END_TABLE if end_game else QUEENS_TABLE
     elif piece_type == chess.KING:
-        mapping = KINGS_END_TABLE if end_game else KINGS_MIDDLE_TABLE
+        _mapping = KINGS_END_TABLE if end_game else KINGS_MIDDLE_TABLE
 
     # Use chess.square_mirror() since mapping[E4] would return the value on the D4 square
     # Reason behind this? No clue, chess module being funny.
 
-    # engine sucks atm
+    mapping = []
+    for k in range(0, 64):
+        mapping.append(_mapping[k] + BASE_SAFETY[k])
 
-    return mapping[chess.square_mirror(square)] if chess.WHITE else mapping[::-1][square]
+    return mapping[chess.square_mirror(square)] if piece.color == chess.WHITE else mapping[square]
 
 
 def is_end_game(BOARD):  # Basic endgame check, if n total pieces are <= 7 then it is an endgame.
@@ -74,7 +76,7 @@ def material_score(BOARD):  # used in depth_handler.py
     return score
 
 
-def evaluate(BOARD, end_game=False, engineType=1):  # Initializes all evaluation functions above
+def evaluate(BOARD, end_game=False, engineType=1):
     # Basic checks for game status
     if BOARD.is_stalemate() | BOARD.is_insufficient_material() | BOARD.is_repetition():
         return 0
@@ -100,19 +102,26 @@ def evaluate(BOARD, end_game=False, engineType=1):  # Initializes all evaluation
             white_attackers = len(BOARD.attackers(chess.WHITE, square))
             black_attackers = len(BOARD.attackers(chess.BLACK, square))
 
-            attack_value = (white_attackers - black_attackers)
+            if white_attackers < black_attackers:
+                attack_value = white_attackers - black_attackers
+            else:
+                attack_value = black_attackers - white_attackers
 
             # Adds the piece weight, mapping values and n attackers based on the piece position
-            val = PIECE_VALUES[piece.piece_type] + square_mapping(piece, square, end_game)
+            val = PIECE_VALUES[piece.piece_type] + square_mapping(piece, square, end_game) + attack_value
             score += val if piece.color == chess.WHITE else -val
-            score += attack_value
+
+            # score += attack_value
         else:  # Faster but less accurate (?) engine
             if piece is None:
                 continue
 
             val = PIECE_VALUES[piece.piece_type] + square_mapping(piece, square, end_game)
             score += val if piece.color == chess.WHITE else -val
+            # print(chess.square_name(square), chess.piece_name(piece.piece_type), piece.color, val)
+            # above value should be zero if symmetrical
 
-    # score += n_moves(BOARD, end_game) if BOARD.turn == chess.WHITE else -n_moves(BOARD, end_game)
+    if engineType == 1:
+        score += n_moves(BOARD, end_game) if BOARD.turn == chess.WHITE else -n_moves(BOARD, end_game)
 
     return score
