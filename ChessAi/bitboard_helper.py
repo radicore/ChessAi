f5165692482e603e8d2124f556c65321a151a55c
radicore <86078764+radicore@users.noreply.github.com>
2023-11-24 19:47:37 +0000

    Update bitboard_helper.py

--- ChessAi/bitboard_helper.py
+++ ChessAi/bitboard_helper.py
@@ -8,14 +8,3 @@ def count_pieces(bb):
 def xray_rook_blockers(BOARD, square):
     return count_pieces(BOARD.occupied & BB_FILES[square_file(square)])-1
 
-
-"""
-def count_bishop_diagonal_blockers(BOARD, square):  # doesn't work. Why? its supposed to be & diagonal_squares not all light / dark squares
-    if r % 2 == 0 and f % 2 != 0:
-    print(f"{square_name(square)} is light")
-    return 8-bin((BOARD.occupied & BB_LIGHT_SQUARES)).count("1")
-else:
-    print(f"{square_name(square)} is dark")
-    return 8-bin((BOARD.occupied & BB_DARK_SQUARES)).count("1")
-"""
-
